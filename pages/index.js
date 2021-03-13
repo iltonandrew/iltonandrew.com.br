ef95e95bbdcba4a62c8201e88d97bc73a5545539
iltonandrew <iltonandrew@gmail.com>
2021-03-13 06:07:29 -0300

    add typewriter :pencil2: :pencil2:

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Typewriter from 'typewriter-effect';
+
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
@@ -13,6 +15,25 @@ export default function Home() {
         <h1 className={styles.title} style={{ color: '#49D49D' }}>
           Olá! Eu sou o Ilton!
         </h1>
+        <div className={styles.subTitle} style={{ color: '#49D49D' }}>
+          <Typewriter
+            options={{
+              cursor: '_',
+            }}
+            onInit={(typewriter) => {
+              typewriter
+
+                .typeString('Eu sou um Engenheiro')
+                .pauseFor(1000)
+                .deleteChars(10)
+                .typeString('Desenvolvedor')
+                .pauseFor(1000)
+                .deleteChars(20)
+                .typeString('construo coisas para a web.')
+                .start();
+            }}
+          />
+        </div>
 
         <p className={styles.description}>
           Bem vindo à minha página!
@@ -46,7 +67,11 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Feito com 💚 por Ilton Andrew
         </a>
       </footer>
