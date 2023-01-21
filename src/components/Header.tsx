f2dc365f54d495715ae7fafdf605ebda26bd500a
Ilton Andrew <iltonandrew@gmail.com>
2023-01-21 17:05:49 -0300

    🛠 fix: images path and scaling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -100,7 +100,7 @@ const DesktopNav = () => {
   const popoverContentBgColor = useColorModeValue("white", "gray.800");
 
   return (
-    <Stack direction={"row"} spacing={4}>
+    <Stack direction={"row"} spacing={2}>
       {NAV_ITEMS.map((navItem) => (
         <Box key={navItem.label}>
           <Popover trigger={"hover"} placement={"bottom-start"}>
