41941f6b5f94d43ed8fd1a87e3015db2288eee57
Ilton Andrew <iltonandrew@gmail.com>
2023-01-22 01:47:51 -0300

    📚meta: add meta data to page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,18 @@ import "../styles/dracula.css";
 import { theme } from "../styles/theme";
 import Header from "../src/components/Header";
 import Footer from "../src/components/Footer";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <meta property="og:title" content="Ilton Andrew" />
+        <meta property="og:description" content="Portifólio & Blog" />
+        <meta property="og:image" content="/images/profile_photo.jpg" />
+        <meta property="og:url" content="https://iltonandrew.com.br" />
+        <meta property="og:type" content="website" />
+      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,13 +7,7 @@ export default function Document() {
   return (
     <Html>
       <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" />
-      <Head>
-        <meta property="og:title" content="Ilton Andrew" />
-        <meta property="og:description" content="Portifólio & Blog" />
-        <meta property="og:image" content="/images/profile_photo.jpg" />
-        <meta property="og:url" content="https://iltonandrew.com.br" />
-        <meta property="og:type" content="website" />
-      </Head>
+      <Head />
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
