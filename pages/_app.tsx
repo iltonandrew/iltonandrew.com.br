984a1d37c63cd6ba133d25bde43cf2636bc320a2
Ilton Andrew <iltonandrew@gmail.com>
2023-01-23 00:12:36 -0300

    💅 styles: change selection color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/dracula.css";
+import "../styles/global.css";
 import { theme } from "../styles/theme";
 import Header from "../src/components/Header";
 import Footer from "../src/components/Footer";
-import Head from "next/head";
 import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
