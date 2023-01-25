ca0572fd1c8ff941b19b083a759a2b94a55ec958
Ilton Andrew <iltonandrew@gmail.com>
2023-01-25 17:30:32 -0300

    📦pkgs: remove next-google-fonts & updates lock

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,10 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import { ColorModeScript } from "@chakra-ui/react";
-import { GoogleFonts } from "next-google-fonts";
 import { theme } from "../styles/theme";
 
 export default function Document() {
   return (
     <Html style={{ scrollBehavior: "smooth" }}>
-      <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" />
       <Head />
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
